8ea547880b6cdea212091fcc3f9ad19d83b4a894
SCIENCE-LEGION\Dael <dael.separa@gmail.com>
2017-11-23 13:21:42 +0100

    fix luminance computation

--- clone/FilterCommon.h
+++ clone/FilterCommon.h
@@ -69,7 +69,7 @@ unsigned char Luminance(int rgb)
 	auto g = (double)Green(rgb);
 	auto b = (double)Blue(rgb);
 
-	return (unsigned char)(0.299 * r + 0.687 * g + 0.114 * b);
+	return (unsigned char)(0.299 * r + 0.587 * g + 0.114 * b);
 }
 
 unsigned char ChromaU(int rgb)
